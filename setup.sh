13ee373dca2d332c2d4489f152272e50f8b385ae
Joel Hutton <Joel.Hutton@arm.com>
2019-12-12 16:05:50 +0000

    setup: Include submodules

--- setup.sh
+++ setup.sh
@@ -9,7 +9,17 @@ do
 	fi
 done
 mkdir -p $HOME/.config/nvim
+mkdir -p $HOME/.config/nvim/bundle/
 ln -sf $dir/init.vim $HOME/.config/nvim/init.vim
+ln -sf $dir/Vundle.vim $HOME/.config/nvim/bundle/Vundle.vim
+
+sudo apt install -y build-essential cmake python3-dev
+ln -sf $dir/YouCompleteMe $HOME/.config/nvim/bundle/YouCompleteMe
+ORIG_DIR=$PWD
+cd $HOME/.config/nvim/bundle/YouCompleteMe
+python3 ./install.py --clang-completer
+cd $ORIG_DIR
+
 ln -sf $dir/vimrc $HOME/.vimrc
 ln -sf $dir/zshrc $HOME/.zshrc
 ln -sf $dir/tmux.conf $HOME/.tmux.conf
